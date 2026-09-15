ba850387eec7bd22e94bf8cea9ddcfdd592fd8ba
AdUhTkJm <2292398666@qq.com>
2026-09-15 13:26:45 +0100

    Slightly more recipes

--- src/main/java/io/aduhtkjm/mekanismheated/registries/ModChemicals.java
+++ src/main/java/io/aduhtkjm/mekanismheated/registries/ModChemicals.java
@@ -13,6 +13,7 @@ public class ModChemicals {
     public static final ChemicalDeferredRegister CHEMICALS = new ChemicalDeferredRegister(Mod.MODID);
 
     public static final DeferredChemical<Chemical> CARBON_MONOXIDE = CHEMICALS.register("carbon_monoxide", 0x4A4A4A);
+    public static final DeferredChemical<Chemical> CARBON_DIOXIDE = CHEMICALS.register("carbon_dioxide", 0x2F2F2F);
     public static final DeferredChemical<Chemical> NITROGEN = CHEMICALS.register("nitrogen", 0x85CBEE);
     public static final DeferredChemical<Chemical> METHANOL = CHEMICALS.register("gas_methanol", 0xCDCDB2);
     public static final DeferredChemical<Chemical> UNSTABLE_LAVA = CHEMICALS.register("gas_unstable_lava", 0xFF8C00);
